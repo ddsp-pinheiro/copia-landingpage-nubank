f9f29591c400ca256537cb4e657ccea607c71a3d
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-03-21 21:13:58 -0300

    feat(js): implementando scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
 let cpf = document.querySelector('#cpf')
+let alert = document.querySelector('#alert')
+let alerti = document.querySelector('#alerti')
 let cpfOk = false
 
 function validaCpf() {
     let txtCpf = document.querySelector('#txtCpf')
-    if (cpf.value.length < 11 ||cpf.value.length >=12 ) {
+    if (cpf.value.length < 11 || cpf.value.length >= 12) {
         txtCpf.innerHTML = 'Precisamos de um CPF válido.'
         txtCpf.style.color = 'red'
     } else {
         txtCpf.innerHTML = ' '
+        txtNome.style.color = 'white'
         cpfOk = true
     }
+}
+
+function aparece(){
+    alert.style.display= 'block'
 
 }
 
-/*function continuar(){
-    if(cpfOk == true){
-        alert('FORMULARIO ENVIADO')
-    }else{
-        alert('PREENCHA O FORMULARIO CORRETAMENTE')
-    }
-}*/
+function some(){
+    alert.style.display= 'none'
+}
+
+
